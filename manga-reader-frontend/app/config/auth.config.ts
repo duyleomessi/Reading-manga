bd81ddc81a052476aa864d77be2ee2e44ce72b5e
duyleomessi <duyleitbka95@gmail.com>
2017-06-01 11:15:24 +0700

    config and image

---
+++ manga-reader-frontend/app/config/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: 'XbVB5Nx5mHdcgJ2CrmW7oW6k4dM2ufPm',
+    domain: 'duyleomessi.auth0.com'
+};
